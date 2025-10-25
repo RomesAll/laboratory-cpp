446246036082df6b86d040babdb5d770a9604c5a
Roman Beskokotov <roma@gmail.com>
2025-10-25 15:45:11 +0300

    Добавил перегрузку для Calculator и проверку типов

--- app/laboratory_work_1/task_1.cpp
+++ app/laboratory_work_1/task_1.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
+#include <typeinfo>
 
 using namespace std;
 
 class Calculator{
     private:
-        int a = 0, b = 0;
-        float a = 0.0f, b = 0.0f;
+        double _a = 0.0, _b = 0.0;
         string** history = new string*[10];
 
         int _factorial_calculation(int n){
@@ -17,31 +17,72 @@ class Calculator{
         }
 
     public:
-        float addition(){
-            return this->a + this->b;
+        Calculator(float a, float b){
+            this->_a = a;
+            this->_b = b;
         }
 
-        float subtraction(){
-            return this->a - this->b;
+        Calculator(string a, float b){
+            try
+            {
+                this->_a = stod(a);
+            }
+            catch(...)
+            {
+                cout << "Couldn't convert variable (a) in integer. To change the values, call the method set_args(a)" << endl;
+            }
         }
 
-        float multiplication(){
-            return this->a * this->b;
+        Calculator(float a, string b){
+            try
+            {
+                this->_b = stod(b);
+            }
+            catch(...)
+            {
+                cout << "Couldn't convert variable (b) in integer. To change the values, call the method set_args(b)" << endl;
+            }
         }
 
-        float division(){
-            return this->a / this->b;
+        Calculator(string a, string b){
+            try
+            {
+                this->_a = stod(a);
+                this->_b = stod(b);
+            }
+            catch(...)
+            {
+                cout << "Couldn't convert variables (a, b) in integers. To change the values, call the method set_args(a, b)" << endl;
+            }
         }
 
-        int* get_factorial_calculation(){
-            int result[2];
-            result[0] = this->_factorial_calculation(this->a);
-            result[1] = this->_factorial_calculation(this->b);
-            return result;
-        }
+        
+
+        // float addition(){
+        //     return this->a + this->b;
+        // }
+
+        // float subtraction(){
+        //     return this->a - this->b;
+        // }
+
+        // float multiplication(){
+        //     return this->a * this->b;
+        // }
+
+        // float division(){
+        //     return this->a / this->b;
+        // }
+
+        // int* get_factorial_calculation(){
+        //     int result[2];
+        //     result[0] = this->_factorial_calculation(static_cast<unsigned int>(this->a));
+        //     result[1] = this->_factorial_calculation(static_cast<unsigned int>(this->b));
+        //     return result;
+        // }
 };
 
 int main() {
-    std::cout << "hello world";
+    Calculator calc('1222', 2);
     return 0;
 }
